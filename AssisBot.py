ee826b43e47a50671a4466aacec0886695b10610
diegosys89 <diego_ssoo@hotmail.com>
2017-11-17 15:03:45 -0500

    Added Telegram messages in case of error founded in the BD

--- AssisBot.py
+++ AssisBot.py
@@ -15,6 +15,7 @@ class AssisBot:
         self.IA = AssisIA()
         self.apikey = config['bot']['apikey']
         self.name = config['bot']['name']
+        self.adminChatId = config['bot']['adminChatId']
         self.updatesDelay = float(config['bot']['delay'])
         self.Telegram = TelegramBot(self.apikey)
         self.Telegram.update_bot_info().wait()
@@ -55,9 +56,24 @@ class AssisBot:
                             file_info = InputFileInfo('NOCData.png', fp, 'image/png')
                             chart = InputFile('photo', file_info)
                             self.Telegram.send_photo(updates[0].message.chat.id, photo=chart).wait()
+                        dataLoadDelta = (datetime.now()-datetime.strptime(res['UpdateTime'],'%a %b %d %H:%M:%S %Y'))
+                        dataLoadTimeHours = dataLoadDelta.seconds / 3600
+                        maxHours = 3
+                        if(dataLoadTimeHours>=maxHours):
+                            msg = "Carga de Datos igual a "+str(dataLoadDelta)+" horas. Revisar BD desactualizada"
+                            self.Telegram.send_message(self.adminChatId, msg).wait()
+                            msg = "Última actualización mayor a 02:30 horas. BD desactualizada, contactar a Administrador"
+                            self.Telegram.send_message(updates[0].message.chat.id, msg).wait()
 
                     print(updates[0].message.text)
                     last_updateId = updates[0].update_id
-            except:
+            except Exception as ex:
+                template = "Un error del tipo {0} ha ocurrido, por favor contactar Administrador. Detalles:\n{1!r}"
+                excepMsg = template.format(type(ex).__name__,ex.args)
                 print("Error capturado a: "+str(datetime.now()))
+                print(excepMsg)
+                if (type(ex).__name__ == "FileNotFoundError"): #Error no se ha encontrado el archivo, contestar con el error
+                    self.Telegram.send_message(updates[0].message.chat.id, excepMsg).wait()
+                    self.Telegram.send_message(self.adminChatId, excepMsg).wait()
+                    last_updateId = updates[0].update_id
                 time.sleep(10)
--- AssisIA.py
+++ AssisIA.py
@@ -7,41 +7,44 @@ class AssisIA:
 
     def getResponse(self, input):
         data = input.message
+        updateTime = self.NocData.getModificationDate()
+        text = ("Hola " + data.sender.first_name)
+        image = False
         if(str(input.message.text).find('tks')>0):
             self.NocData.loadData()
             num = self.NocData.get_nocQTY()
             goal = self.NocData.getMeta()
-            updateTime = self.NocData.getModificationDate()
-            text = ("Hola " + data.sender.first_name + ", Tenemos " + str(num) + " tickets. Actualizado al: "+updateTime+
+            text = (text + ", Tenemos " + str(num) + " tickets. Actualizado al: "+updateTime+
                     ". Meta para el día de hoy "+str(goal)+" tickets.")
-            msg = {'Image':False,'ImagePath':False,'Text':text}
-            return msg
+            imagePath = False
 
         elif(str(input.message.text).find('top')>0):
             self.NocData.loadData()
             resp = self.NocData.get_nocTOP()
-            updateTime = self.NocData.getModificationDate()
-            text = ("Hola "+data.sender.first_name+"\nActualizado al: "+updateTime)
-            msg = {'Image':True,'ImagePath':resp['ImagePath'],'Text':text}
-            return msg
+            text = (text+"\nActualizado al: "+updateTime)
+            image = True
+            imagePath = resp['ImagePath']
+
         elif(str(input.message.text).find('fse')>0):
             self.NocData.loadData()
             resp = self.NocData.get_nocFSE()
-            updateTime = self.NocData.getModificationDate()
-            text = ("Hola "+data.sender.first_name+"\nActualizado al: "+updateTime)
-            msg = {'Image':True,'ImagePath':resp['ImagePath'],'Text':text}
-            return msg
+            text = (text + "\nActualizado al: "+updateTime)
+            image = True
+            imagePath = resp['ImagePath']
+
         elif(str(input.message.text).find('tre')>0):
             self.NocData.loadData()
             resp = self.NocData.get_nocTRE()
-            updateTime = self.NocData.getModificationDate()
-            text = ("Hola "+data.sender.first_name+"\nActualizado al: "+updateTime)
-            msg = {'Image':True,'ImagePath':resp['ImagePath'],'Text':text}
-            return msg
+            text = (text + "\nActualizado al: "+updateTime)
+            image = True
+            imagePath = resp['ImagePath']
+
         else:
-            text = ("Hola "+data.sender.first_name+", no tengo respuesta para tu petición")
-            msg = {'Image':False,'ImagePath':None,'Text':text}
-            return msg
+            text = (text + ", no tengo respuesta para tu petición")
+            imagePath = None
+
+        msg = {'Image':image,'ImagePath':imagePath,'Text':text,'UpdateTime':updateTime}
+        return msg
 
 #Primer nombre: data.sender.first_name
 #Apellid: data.sender.last_name
