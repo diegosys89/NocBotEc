2e5c450dacb4f601bbfbfe78130171f19d363712
diegosys89 <diego_ssoo@hotmail.com>
2017-12-20 10:13:15 -0500

    Added auto init option

--- main.py
+++ main.py
@@ -2,18 +2,14 @@
 
 from AssisBot import AssisBot
 import time
+import sys
 
 def main():
     Bot = AssisBot()
     Connection = Bot.Telegram
     Connection.update_bot_info().wait()
-    option = 'x'
+    option = sys.argv[1] if len(sys.argv)>1 else 'x'
     while option != '0':
-        print('------------Menu principal AssisBot--------------')
-        print('1. Start NocBot')
-        print('2. Pause NocBot')
-        print('0. Exit')
-        option = input()
         if option == '1':
             Bot.startToListen()
         elif option == '2':
@@ -21,6 +17,12 @@ def main():
         elif option == '0':
             Bot.stopToListen()
             exit()
+        print('------------Menu principal AssisBot--------------')
+        print('1. Start NocBot')
+        print('2. Pause NocBot')
+        print('0. Exit')
+        option = input()
+
 
 if __name__ == "__main__":
     main()
